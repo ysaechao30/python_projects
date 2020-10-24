bd3c647a99b013ddf88dfadf02935bf2810305ec
ysaechao30 <59863962+ysaechao30@users.noreply.github.com>
2020-10-24 14:09:43 -0700

    Add files via upload

---
+++ ClassInherAssn.py
@@ -0,0 +1,14 @@
+class School:
+    company = "UCLA"
+    location = "Los Angeles"
+    email = 'admissions@ucla.edu'
+    number = 123-156-1235
+
+class Teacher(School):
+    Department = 'Physics'
+    Pay = 1.00
+
+class Student(School):
+    Name = 'Suzy Stark'
+    Major = 'Music'
+    
